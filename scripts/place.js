b250d7c2176bb88588a0008003284cd364073271
SilviaCastagnino <sylvia19976@gmail.com>
2025-05-24 23:13:56 +0200

    New changes

--- scripts/place.js
+++ scripts/place.js
@@ -1,7 +1,22 @@
 document.getElementById('currentyear').textContent = new Date().getFullYear();
 document.getElementById('lastModified').textContent = `Last Modified: ${document.lastModified}`;
 
+function calculateWindChill(temp, windSpeed) {
+    return 13.12 + 0.6215 * temp - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temp * Math.pow(windSpeed, 0.16);
+}
+
 function displayData() {
+
+    const temperature = 8;
+    const windSpeed = 12;
+    const conditions = "Partly cloudy";
+
+
+    let windChill = "N/A";
+    if (temperature <= 10 && windSpeed > 4.8) {
+        windChill = calculateWindChill(temperature, windSpeed).toFixed(1) + "°C";
+    }
+
     document.getElementById('data').innerHTML = `
     <h2>Data</h2>
     <strong>Area:</strong><span>302.073 sq km</span>
@@ -19,10 +34,10 @@ function displayData() {
         <p>⛅</p>
         <img src="images/wheater-cloud.svg" alt="weather cloud">
     </div>
-    <strong>Temperature:</strong> <span>....</span>
-    <strong>Conditions:</strong><span>......</span>
-    <strong>Wind:</strong><span>Rome</span>
-    <strong>Wind chill:</strong><span>.....</span>
+    <strong>Temperature:</strong> <span>${temperature}°C</span>
+    <strong>Conditions:</strong><span>${conditions}</span>
+    <strong>Wind:</strong><span>${windSpeed} km/h</span>
+    <strong>Wind chill:</strong><span>${windChill}</span>
     `;
 }
 
