e603904610b36cb009460a171597ba4da50f6518
SilviaCastagnino <sylvia19976@gmail.com>
2025-05-24 22:12:13 +0200

    New Files

---
+++ scripts/place.js
@@ -0,0 +1,28 @@
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+document.getElementById('lastModified').textContent = `Last Modified: ${document.lastModified}`;
+
+function displayData() {
+    document.getElementById('data').innerHTML = `
+    <h2>Data</h2>
+    <strong>Area:</strong><span>302.073 sq km</span>
+    <strong>Population:</strong><span>58.990.000</span>
+    <strong>Capital:</strong><span>Rome</span>
+    <strong>Languages:</strong><span>Italian</span>
+    <strong>Currency:</strong><span>Euro</span>
+    <strong>Time zone:</strong><span>Winter UTC+1, Summer UTC+2</span>
+    <strong>Calling Code:</strong><span>+39</span>
+    <strong>Internet TLD:</strong><span>.it</span>
+    `;
+    document.getElementById('weather').innerHTML = `
+    <div class="title">
+        <h2>Weather </h2>
+        <img src="images/wheater cloud.svg" alt="weather cloud">
+    </div>
+    <strong>Temperature:</strong> <span>....</span>
+    <strong>Conditions:</strong><span>......</span>
+    <strong>Wind:</strong><span>Rome</span>
+    <strong>Wind chill:</strong><span>.....</span>
+    `;
+}
+
+displayData();
